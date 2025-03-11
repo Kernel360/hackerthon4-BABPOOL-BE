27b40399c33de93d8c741c3c9e46b290658b039a
suyeon <whtndus0809@naver.com>
2025-03-11 17:41:50 +0900

    변경내용 저장

--- src/main/java/org/example/hana/recruitment/entity/Application.java
+++ src/main/java/org/example/hana/recruitment/entity/Application.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
-import org.example.hana.BaseEntity;
+import org.example.hana.global.BaseEntity;
 import org.example.hana.user.entity.User;
 
 @NoArgsConstructor
