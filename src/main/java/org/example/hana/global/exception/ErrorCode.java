c041d54a1708fef11fe927ee35136b431db512b4
yoodab <yoondab2166@gmail.com>
2025-03-11 12:02:56 +0900

    feat: ErrorCode 추가

--- src/main/java/org/example/hana/global/exception/ErrorCode.java
+++ src/main/java/org/example/hana/global/exception/ErrorCode.java
@@ -17,6 +17,22 @@ public enum ErrorCode {
 
     // 각 Service에서 필요한 ErrorCode 추가
 
+    //
+    // Recruitment (공고)
+    RECRUITMENT_POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 공고를 찾을 수 없습니다."),
+    RECRUITMENT_COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 공고를 찾을 수 없습니다."),
+    RECRUITMENT_POST_MAX_PARTICIPANTS_EXCEEDED(HttpStatus.BAD_REQUEST, "최대 참여 인원을 초과했습니다."),
+
+    //
+
+    APPLICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 지원요청을 찾을 수 없습니다."),
+
+
+
+    NO_PERMISSION_TO_MODIFY(HttpStatus.FORBIDDEN, "수정 권한이 없습니다."), // 추가된 에러 코드
+
+
+
 
     // Auth
     DUPLICATE_USER(HttpStatus.CONFLICT, "이미 존재하는 아이디입니다."),
